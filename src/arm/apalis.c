649f37d9b75a7a8d0367ed67afaa37ebe4e9b7e3
Andy Doan <andy@foundries.io>
2019-03-19 14:21:35 -0500

    apalis: Populate a few GPIOs
    Signed-off-by: Andy Doan <andy@foundries.io>

--- src/arm/apalis.c
+++ src/arm/apalis.c
@@ -32,6 +32,14 @@
 
 #define PLATFORM_NAME_APALIS "Apalis"
 
+static int GPIOS[][2] = {
+    { 2, 4 }, //GPIO2_IO4
+    { 2, 5 },
+    { 2, 6 },
+    { 2, 7 },
+    { 6, 10 },  //GPIO6_IO10
+};
+
 mraa_board_t*
 mraa_apalis()
 {
@@ -40,7 +48,7 @@ mraa_apalis()
         return NULL;
     }
     b->platform_name = PLATFORM_NAME_APALIS;
-    b->phy_pin_count = 0;
+    b->phy_pin_count = sizeof(GPIOS) / (2 * sizeof(int));
 
     b->pins = (mraa_pininfo_t*) calloc(b->phy_pin_count, sizeof(mraa_pininfo_t));
     if (b->pins == NULL) {
@@ -49,6 +57,13 @@ mraa_apalis()
     }
 
     b->gpio_count = 0;
+    int i;
+    for (i = 0; i < b->phy_pin_count; i++) {
+        snprintf(b->pins[i].name, MRAA_PIN_NAME_SIZE, "GPIO%d_IO%d", GPIOS[i][0], GPIOS[i][1]);
+        b->pins[i].capabilities = (mraa_pincapabilities_t){ 1, 1, 0, 0, 0, 0, 0, 0 };
+        b->pins[i].gpio.pinmap = ((GPIOS[i][0] - 1) * 32) + GPIOS[i][1];
+        b->gpio_count++;
+    }
 
     return b;
 }
